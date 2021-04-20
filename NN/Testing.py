3916db02d97a0f501e80585599a02e06be204ce2
Kurosaki <ayvak@github.com>
2021-04-20 15:06:00 +0300

    added output layer

--- NN/Testing.py
+++ NN/Testing.py
@@ -20,6 +20,80 @@ import gradio as gr
 from sklearn import preprocessing
 
 model = load_model("PDP_NN")
+
+#printing most probable number
+def probable_number(output,max_values,number,col):
+    #number = 0 -----> Most Probable
+    #number = 1 -----> Second Most Probable
+    #number = 2 -----> Third Most Probable
+    
+    #output - LED matrix
+    
+    #max_values - list of tuples containing the maximum probable numbers and their probabilities
+    
+    #col - What colour (number) needs to be put for the respective layer
+    if max_values[number][1] == 0:
+        output[2 + number*9 ,1:6]=col
+        output[8 + number*9 ,1:6]=col
+        output[2 + number*9:9 + number*9 ,1]=col
+        output[2+ number*9:9+ number*9 ,5]=col
+
+    if max_values[number][1] == 1:
+        output[2+ number*9:9+ number*9 ,5]=col
+
+    if max_values[number][1] == 2:   
+        output[2+ number*9 ,1:6]=col
+        output[5+ number*9 ,1:6]=col
+        output[8+ number*9 ,1:6]=col
+        output[2+ number*9:5+ number*9 ,5]=col
+        output[5+ number*9:9+ number*9 ,1]=col
+
+    if max_values[number][1] == 3:   
+        output[2+ number*9 ,1:6]=col
+        output[5+ number*9 ,1:6]=col
+        output[8+ number*9 ,1:6]=col
+        output[2+ number*9:9+ number*9 ,5]=col
+
+    if max_values[number][1] == 4:
+        output[5+ number*9,1:6]=col
+        output[2+ number*9:9+ number*9,5]=col
+        output[2+ number*9:5+ number*9,1]=col
+
+    if max_values[number][1] == 5:   
+        output[2+ number*9,1:6]=col
+        output[5+ number*9,1:6]=col
+        output[8+ number*9,1:6]=col
+        output[2+ number*9:5+ number*9,1]=col
+        output[5+ number*9:9+ number*9,5]=col
+
+    if max_values[number][1] == 6:   
+        output[2+ number*9,1:6]=col
+        output[5+ number*9,1:6]=col
+        output[8+ number*9,1:6]=col
+        output[2+ number*9:9+ number*9,1]=col
+        output[5+ number*9:9+ number*9,5]=col
+
+    if max_values[number][1] == 7:  
+        output[2+ number*9,1:6]=col
+        output[2+ number*9:9+ number*9,5]=col
+
+    if max_values[number][1] == 8:
+        output[2+ number*9,1:6]=col
+        output[5+ number*9,1:6]=col
+        output[8+ number*9,1:6]=col
+        output[2+ number*9:9+ number*9,1]=col
+        output[2+ number*9:9+ number*9,5]=col
+
+    if max_values[number][1] == 9:
+        output[2+ number*9,1:6]=col
+        output[5+ number*9,1:6]=col
+        output[8+ number*9,1:6]=col
+        output[2+ number*9:5+ number*9,1]=col
+        output[2+ number*9:9+ number*9,5]=col
+        
+    #probability of the numbers
+    output[4+number*9:7+number*9,8:9+round(max_values[number][0]*18)]=col
+    
 def classify(image):
     #print(image.shape)
     image = image/255.0
@@ -37,10 +111,17 @@ def classify(image):
         matplotlib.image.imsave('outfile.jpg',image)
     image = image.reshape(1,28,28,1)
     prediction = model.predict(image).tolist()[0]
-    print(f"prediction : {prediction}")
+    output = np.array([[0 for col in range(28)] for row in range(28)])
+    max_values = sorted(zip(prediction,range(10)),reverse=True)[:3]
+    probable_number(output,max_values,0,1) #colour for most probable number
+    probable_number(output,max_values,1,1) #colour for second most probable number
+    probable_number(output,max_values,2,1) #colour for third most probable number
+    
+    #output is our required matrix
     return {str(i): prediction[i] for i in range(10)}
+
 sketchpad = gr.inputs.Sketchpad()
 label = gr.outputs.Label(num_top_classes=3)
-interface = gr.Interface(classify, sketchpad, label, live=True, capture_session=True)
+interface = gr.Interface(classify, sketchpad, label, live=True,interpretation="default", capture_session=True)
 
 interface.launch(share=True)
